1dfb6e1b040819cb852fa899e5f8d7e4897bd9cf
hoanguyen81190 <hoanguyen81190@gmail.com>
2019-05-23 11:40:18 +0200

    added missing files

---
+++ WebEntry/src/components/Views/GazeCursor.js
@@ -0,0 +1,76 @@
+import React, { PropTypes } from 'react';
+import './GazeCursor.css';
+
+import store from '../../core/store';
+
+//var key = require('keymaster');
+
+class GazeCursor extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      visible: true,
+      locX: 0,
+      locY: 0
+    }
+    this.handleCursorVisibility = this.toggleVisibility.bind(this);
+    this.handleGazeLocUpdate = this.updateCursorLocation.bind(this);
+    this.cursorRadius = 50;
+
+    let radiusAction = {
+      type: 'SET_GAZE_RADIUS',
+      gazeRadius: this.cursorRadius
+    }
+    store.dispatch(radiusAction);
+  }
+
+  componentDidMount() {
+    // key.setScope('stimuli');
+    // key('g', this.handleCursorVisibility);
+    this.timer = setInterval(this.handleGazeLocUpdate, 4.5); //Update the gaze cursor location every 2ms
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.timer);
+    // key.unbind('g');
+  }
+
+  render() {
+    let cursor = null;
+    if(this.state.visible){
+      cursor = <div className="gazeCursor" id="gazeCursorDiv"/>;
+    }
+
+    return (
+      cursor
+    );
+  }
+
+  updateCursorLocation(){
+    let gazeLoc = store.getState().gazeData;
+    //Only draw the cursor if it is visible
+    if(this.state.visible){
+      var cursorDiv = document.getElementById("gazeCursorDiv");
+      cursorDiv.style.left = (gazeLoc.locX*this.props.viewWidth-this.cursorRadius)+'px';
+      cursorDiv.style.top = (gazeLoc.locY*this.props.viewHeight-this.cursorRadius)+'px';
+      cursorDiv.style.width = this.cursorRadius*2+"px";
+      cursorDiv.style.height = this.cursorRadius*2+"px";
+    }
+
+    this.setState({
+      locX: gazeLoc.locX,
+      locY: gazeLoc.locY
+    })
+  }
+
+  toggleVisibility(){
+    console.log("Gaze cursor visibility: " + !this.state.visible);
+    this.setState(
+      {
+        visible: !this.state.visible
+      }
+    );
+  }
+}
+
+export default GazeCursor;
