d8fcfcb01c306a02c07438f90ece7aadd688752b
Jens-Patrick <jens.patrick.langstrand@ife.no>
2019-10-25 12:57:42 +0200

    Constrained the gaze cursor inside the box

--- WebEntry/src/components/Views/GazeCursor.js
+++ WebEntry/src/components/Views/GazeCursor.js
@@ -35,21 +35,33 @@ class GazeCursor extends React.Component {
     clearInterval(this.timer);
   }
 
+  clamp(val, min, max){
+      return Math.min(Math.max(min, val), max);
+  }
+
   updateCursorLocation(){
     try {
       let gazeLoc = store.getState().gazeData[this.props.tracker];
+      let gazeLocX = this.clamp(gazeLoc.locX, 0, 1) * this.frameDiv.current.offsetWidth;
+      gazeLocX = gazeLocX > this.frameDiv.current.offsetWidth-this.cursorRadius*2
+        ? this.frameDiv.current.offsetWidth-this.cursorRadius*2 : gazeLocX;
+
+      let gazeLocY = this.clamp(gazeLoc.locY, 0, 1) * this.frameDiv.current.offsetHeight;
+      gazeLocY = gazeLocY > this.frameDiv.current.offsetHeight-this.cursorRadius*2
+        ? this.frameDiv.current.offsetHeight-this.cursorRadius*2 : gazeLocY;
+
       //Only draw the cursor if it is visible
       if(this.state.visible && this.frameDiv){
         var cursorDiv = document.getElementById("gazeCursorDiv" + this.props.id);
-        cursorDiv.style.left = (gazeLoc.locX*this.frameDiv.current.offsetWidth-this.cursorRadius)+'px';
-        cursorDiv.style.top = (gazeLoc.locY*this.frameDiv.current.offsetHeight-this.cursorRadius)+'px';
+        cursorDiv.style.left = gazeLocX+'px';
+        cursorDiv.style.top = gazeLocY+'px';
         cursorDiv.style.width = this.cursorRadius*2+"px";
         cursorDiv.style.height = this.cursorRadius*2+"px";
       }
 
       this.setState({
-        locX: gazeLoc.locX,
-        locY: gazeLoc.locY
+        locX: gazeLocX,
+        locY: gazeLocY
       })
     } catch (err) {
 
@@ -75,7 +87,7 @@ class GazeCursor extends React.Component {
     let bgolor = theme.palette.type === "light" ? theme.palette.primary.dark : theme.palette.primary.main;
 
     return (
-      <div style={{backgroundColor: bgolor}} className="wrapper" >
+      <div style={{backgroundColor: bgolor}} className="ETwrapper" >
         <div className="title"><Typography variant="body1" color="textPrimary" align="center">{this.props.participant}</Typography></div>
         <div className="gazeCursorContainer" ref={this.frameDiv}>
           {cursor}
