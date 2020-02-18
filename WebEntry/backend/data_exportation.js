db95b73354f0cd7b753ce421e14aeaa7cde9eb1a
hoanguyen81190 <hoanguyen81190@gmail.com>
2020-02-18 16:10:07 +0100

    fixed small bugs

--- WebEntry/backend/data_exportation.js
+++ WebEntry/backend/data_exportation.js
@@ -80,21 +80,35 @@ exports.get_gaze_data = function (participantId) {
 ██████   ██████   ███ ███  ██   ████ ███████  ██████  ██   ██ ██████  ██   ██ ██████  ███████ ███████
 */
 
+function formatDateTime(t) {
+  var d = new Date();
+  var fillZero = (num) => {
+    if (num < 10) {
+      return '0' + num;
+    }
+    else {
+      return num;
+    }
+  }
+  var datestring = d.getFullYear() + '-' + fillZero(d.getMonth()) + '-' + fillZero(d.getDate())
+                    + '_' + fillZero(d.getHours()) + '-' + fillZero(d.getMinutes());
+  return datestring;
+}
+
 exports.save_to_csv = async function(p) {
     var globalVariables = "";
     var file_name = "";
 
     if(p.linesOfData && p.linesOfData.length > 0){
-      file_name = p.linesOfData[0].tasksFamilyTree[0] + '_';
-      date = new Date(p.linesOfData[0].startTimestamp);
-      file_name += date.toUTCString().replace(/\s/g,'') +"_";
+      file_name = formatDateTime(p.linesOfData[0].startTaskTime) + '_';
+      file_name += p.linesOfData[0].tasksFamilyTree[0] + '_';
     }
 
     for (let i = 0; i < p.globalVariables.length; i++) {
       /*header += p.globalVariables[i].label + ",";*/
       if (!p.globalVariables[i].label.toLowerCase().includes("record data")) {
         globalVariables += p.globalVariables[i].label + '_' + p.globalVariables[i].value + ":"; /* Was "," but that does not make sense*/
-        file_name += p.globalVariables[i].label + '_' + p.globalVariables[i].value + '_';
+        file_name += p.globalVariables[i].label + '-' + p.globalVariables[i].value + '_';
       }
     }
 
