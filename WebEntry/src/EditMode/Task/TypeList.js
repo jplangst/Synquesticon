51245a4ce51b518881b07689b61b997edfab2012
michaelhil <51119446+michaelhil@users.noreply.github.com>
2020-04-24 23:47:31 +0200

    Update TypeList.js

--- WebEntry/src/EditMode/Task/TypeList.js
+++ WebEntry/src/EditMode/Task/TypeList.js
@@ -1,19 +1,14 @@
-import React, { Component } from 'react';
-
-import TaskTypeItem from './TypeItem';
-
+import React from 'react';
 import { Droppable, Draggable } from 'react-beautiful-dnd';
 import styled from 'styled-components';
-
 import { withTheme } from '@material-ui/styles';
-
-import './TypeList.css';
-
 import Button from '@material-ui/core/Button';
 import DragIcon from '@material-ui/icons/ControlCamera';
 import { Typography } from '@material-ui/core';
 
-/* Used to leave a visual clone behind during drag */
+import './TypeList.css';
+import TaskTypeItem from './TypeItem';
+
 const TaskClone = styled.div`
     display: flex;
     user-select: none;
@@ -27,67 +22,55 @@ const Clone = styled(TaskClone)`
     }
 `;
 
-//================ React component ================
-class EditTaskTypeList extends Component {
-  constructor(props) {
-    super(props);
-    this.taskList = props.taskList;
-  }
+const EditTaskTypeList = props => {
+  const taskList = props.taskList;
+  const { theme } = props;
+  let bgColor = theme.palette.type === "light" ? theme.palette.primary.dark : theme.palette.primary.main;
 
-  //-----------Tasks------------
-  render() {
-    this.taskList = this.props.taskList;
-
-    const { theme} = this.props;
-    let bgColor = theme.palette.type === "light" ? theme.palette.primary.dark : theme.palette.primary.main;
-
-    return(
-      <Droppable droppableId={this.props.droppableId} isDropDisabled={true} >
-       {(provided, snapshot) => (
-        <div className="synquestitaskListComponentContainer" ref={provided.innerRef}>
-          {
-            this.taskList.map((item, index) => {
-              var margin = index < this.taskList.length-1 ? " synquestiMargin":"";
-
-              var clonedContent= <div
-                className={"synquestiListItem " + margin} style={{backgroundColor:bgColor}}>
-                <div className="synquestiListItemTextContainer" >
-                  <div className="synquestiListItemText">
-                    <Typography color="textPrimary" noWrap> {item.label} </Typography>
-                  </div>
+  return(
+    <Droppable droppableId={props.droppableId} isDropDisabled={true} >
+      {(provided, snapshot) => (
+      <div className="synquestitaskListComponentContainer" ref={provided.innerRef}>
+        {
+          taskList.map((item, index) => {
+            const margin = index < taskList.length-1 ? " synquestiMargin":"";
+            const clonedContent= <div
+              className={"synquestiListItem " + margin} style={{backgroundColor:bgColor}}>
+              <div className="synquestiListItemTextContainer" >
+                <div className="synquestiListItemText">
+                  <Typography color="textPrimary" noWrap> {item.label} </Typography>
                 </div>
-                <div className="synquestiListItemDragBtnContainer" style={{backgroundColor:bgColor}}>
-                  <Button style={{cursor:'move',width: '100%', height: '100%', minWidth: '30px', minHeight: '30px'}}
-                    className="synquestiListItemDragBtn" size="small" fullWidth >
-                    <DragIcon className="synquestiDragBtnIcon"/>
-                  </Button>
-                </div>
-              </div>;
+              </div>
+              <div className="synquestiListItemDragBtnContainer" style={{backgroundColor:bgColor}}>
+                <Button style={{cursor:'move',width: '100%', height: '100%', minWidth: '30px', minHeight: '30px'}}
+                  className="synquestiListItemDragBtn" size="small" fullWidth >
+                  <DragIcon className="synquestiDragBtnIcon"/>
+                </Button>
+              </div>
+            </div>;
 
-              return(
-                <Draggable key={index} draggableId={item+index} index={index} shouldRespectForceTouch={false} isDragDisabled={!this.props.dragEnabled}>
-                {(provided, snapshot) => (
-
-                  <React.Fragment>
-                    <TaskTypeItem domRef={provided.innerRef} provided={provided} dragEnabled={this.props.dragEnabled}
-                      isDragging={snapshot.isDragging} snapshot={snapshot}
-                      placeholder={false} task={item} itemType={this.props.itemType}
-                      handleDrop={this.props.dragDropCallback} content={item.label} marginClass={margin}
-                    />
-                    {snapshot.isDragging && (
-                      <Clone>{clonedContent}</Clone>
-                    )}
-                  </React.Fragment>
-                )}
-                </Draggable>
-              );
-            })}
-             <div style={{display: 'none'}}>{provided.placeholder}</div>
-          </div>
-      )}
-      </Droppable>
-    );
-  }
+            return(
+              <Draggable key={index} draggableId={item+index} index={index} shouldRespectForceTouch={false} isDragDisabled={!props.dragEnabled}>
+              {(provided, snapshot) => (
+                <React.Fragment>
+                  <TaskTypeItem domRef={provided.innerRef} provided={provided} dragEnabled={props.dragEnabled}
+                    isDragging={snapshot.isDragging} snapshot={snapshot}
+                    placeholder={false} task={item} itemType={props.itemType}
+                    handleDrop={props.dragDropCallback} content={item.label} marginClass={margin}
+                  />
+                  {snapshot.isDragging && (
+                    <Clone>{clonedContent}</Clone>
+                  )}
+                </React.Fragment>
+              )}
+              </Draggable>
+            );
+          })}
+            <div style={{display: 'none'}}>{provided.placeholder}</div>
+        </div>
+    )}
+    </Droppable>
+  );
 }
 
 export default withTheme(EditTaskTypeList);
