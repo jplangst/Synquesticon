86bbff43d14979089c7d652025a5f61a089bcce0
michaelhil <51119446+michaelhil@users.noreply.github.com>
2020-04-25 21:40:43 +0200

    Update ComponentItem.js

--- WebEntry/src/EditMode/Task/ComponentItem.js
+++ WebEntry/src/EditMode/Task/ComponentItem.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import TextField from '@material-ui/core/TextField';
 import Checkbox from '@material-ui/core/Checkbox';
@@ -10,119 +10,87 @@ import * as dbObjects from '../../core/db_objects';
 import db_helper from '../../core/db_helper';
 
 var _id = 0;
-class TaskComponentItem extends Component {
-  constructor(props){
-    super(props);
+const TaskComponentItem = props => {
+  const [singleChoice, setSingleChoice] = useState(props.task.singleChoice);
+  const [resetResponses, setResetResponses] = useState(props.task.resetResponses);
+  const [globalVariable, setGlobalVariable] = useState(props.task.globalVariable ? props.task.globalVariable : false);
 
-    this.state = {
-      singleChoice: props.task.singleChoice,
-      resetResponses: props.task.resetResponses,
-      globalVariable: props.task.globalVariable ? props.task.globalVariable : false
-    };
-
-    //Image
-    this.shouldUpload = false;
-    this.imageToUpload = null;
-    this.imageSelectCallback = this.onSelectImage.bind(this);
-
-    //Task update
-    this.responseHandler = this.onResponsesChanged;
-    this.handleGlobalVariableChanged = this.onGlobalVariableChanged.bind(this);
-    this.handleSingleChoiceChanged = this.onSingleChoiceChanged.bind(this);
-    this.handleResetResponsesChanged = this.onResetResponsesChanged.bind(this);
-
-    this.uniqueID = _id;
-    _id++;
-  }
-
-  onTaskChanged(){
-    console.log("task changed");
-    console.log(this.props.task.objType, this.shouldUpload);
-    if (this.props.task.objType === dbObjects.TaskTypes.IMAGE && this.shouldUpload) {
-      this.uploadImages();
-    }
-  }
+  //Image
+  let shouldUpload = false;
+  let imageToUpload = null;
+  let uniqueID = _id;
+  _id++;
 
   //Callback form the SynquestiImage component
-  onSelectImage(shouldUpload, image) {
-    this.shouldUpload = shouldUpload;
-    this.imageToUpload = image;
-
-    console.log("HERE");
-
-    if (this.shouldUpload) {
-      console.log("HERE");
-      this.uploadImages();
+  const onSelectImage = (shouldUpload, image) => {
+    shouldUpload = shouldUpload;
+    imageToUpload = image;
+    if (shouldUpload) {
+      uploadImages();
     }
   }
   //Upload the sleected image to the database
-  uploadImages() {
-    if (this.imageToUpload) {
+  const uploadImages = () => {
+    if (imageToUpload) {
       const formData = new FormData();
-      formData.append('images',this.imageToUpload);
+      formData.append('images',imageToUpload);
       const config = {
           headers: {
               'content-type': 'multipart/form-data'
           }
       };
-
-      db_helper.uploadImage(this.imageToUpload, formData, config, null);
+      db_helper.uploadImage(imageToUpload, formData, config, null);
     }
   }
 
-  onGlobalVariableChanged(e, checked){
-    this.props.task.globalVariable = checked;
-    this.setState({
-      globalVariable: checked,
-    });
+  const onGlobalVariableChanged = (e, checked) => {
+    props.task.globalVariable = checked;
+    setGlobalVariable(checked);
   }
 
   //Get the components to render based on the object type
-  getComponent(){
+  const getComponent = () => {
     let component = null;
-    switch (this.props.task.objType){
+    switch (props.task.objType){
       case dbObjects.TaskTypes.INSTRUCTION.type: {
         component =  <div>
                 <TextField
                   required
                   padding="dense"
-                  id={this.uniqueID+"instructionText"}
-                  defaultValue={this.props.task.displayText}
+                  id={uniqueID+"instructionText"}
+                  defaultValue={props.task.displayText}
                   placeholder="Write your instruction here"
                   label="Instructions"
-                  ref="instructionTextRef"
                   fullWidth
                   multiline
                   rows="1"
-                  onChange={(e)=>{this.props.task.displayText = e.target.value}}
+                  onChange={(e)=>{props.task.displayText = e.target.value}}
                 />
                 <TextField label="Screen IDs"
                    required
                    padding="dense"
                    style={{width:"100px"}}
-                   id={this.uniqueID+"insScreenIDs"}
-                   defaultValue={this.props.task.screenIDS.join(',')}
+                   id={uniqueID+"insScreenIDs"}
+                   defaultValue={props.task.screenIDS.join(',')}
                    placeholder="1, 2"
-                   ref="insScreenIDRef"
-                   onChange={(e)=> this.responseHandler(e, e.target.value, "ScreenIDs")}
+                   onChange={(e)=> onResponsesChanged(e, e.target.value, "ScreenIDs")}
                  />
               </div>;
         break;
       }
       case dbObjects.TaskTypes.IMAGE.type: {
         component = <div>
-                      <ImageTaskType task={this.props.task}
-                         selectImageCallback={this.imageSelectCallback}
-                         uniqueID={this.uniqueID+"image"}/>
+                      <ImageTaskType task={props.task}
+                         selectImageCallback={onSelectImage}
+                         uniqueID={uniqueID+"image"}/>
                       <TextField label="Screen IDs"
                          required
                          padding="dense"
                          style={{width:"100px"}}
-                         id={this.uniqueID+"imageScreenIDs"}
-                         defaultValue={this.props.task.screenIDS.join(',')}
+                         id={uniqueID+"imageScreenIDs"}
+                         defaultValue={props.task.screenIDS.join(',')}
                          placeholder="1, 2"
-                         ref="imageScreenIDRef"
-                         onChange={(e)=> this.responseHandler(e, e.target.value, "ScreenIDs")}
+                         onChange={(e)=> onResponsesChanged(e, e.target.value, "ScreenIDs")}
                        />
                    </div>
         break;
@@ -133,70 +101,66 @@ class TaskComponentItem extends Component {
             required
             padding="dense"
             fullWidth
-            id={this.uniqueID+"mcQuestionText"}
-            defaultValue={this.props.task.displayText}
+            id={uniqueID+"mcQuestionText"}
+            defaultValue={props.task.displayText}
             placeholder="Enter your question here"
-            ref="questionTextRef"
             multiline
             rows="1"
-            onChange={(e)=>{this.props.task.displayText = e.target.value}}
+            onChange={(e)=>{props.task.displayText = e.target.value}}
           />
           <TextField label="Answers(comma-separated)"
             required
             padding="dense"
             style={{marginRight:"10px", width:"calc(50% - 10px)"}}
-            id={this.uniqueID+"mcResponses"}
-            defaultValue={this.props.task.responses.join(',')}
+            id={uniqueID+"mcResponses"}
+            defaultValue={props.task.responses.join(',')}
             placeholder="Answer A, Answer B, Answer C"
-            ref="responsesRef"
-            onChange={(e)=> this.responseHandler(e, e.target.value, "Responses")}
+            onChange={(e)=> onResponsesChanged(e, e.target.value, "Responses")}
           />
           <TextField label="Correct answers(comma-separated)"
             required
             padding="dense"
             style={{width:"50%"}}
-            id={this.uniqueID+"mcCorrectResponses"}
-            defaultValue={this.props.task.correctResponses.join(',')}
+            id={uniqueID+"mcCorrectResponses"}
+            defaultValue={props.task.correctResponses.join(',')}
             placeholder="Answer A, Answer C"
-            ref="correctResponseRef"
-            onChange={(e)=> this.responseHandler(e, e.target.value, "Correct Responses")}
+            onChange={(e)=> onResponsesChanged(e, e.target.value, "Correct Responses")}
           />
           <FormControlLabel label="Single Choice"
             value="end"
-            id={this.uniqueID+"schoice"}
+            id={uniqueID+"schoice"}
             padding="dense"
-            checked={this.state.singleChoice}
+            checked={singleChoice}
             control={<Checkbox  color="secondary" />}
-            onChange={this.handleSingleChoiceChanged}
+            onChange={onSingleChoiceChanged}
             labelPlacement="end"
           />
           <FormControlLabel label="Auto-Reset"
             value="end"
-            id={this.uniqueID+"reset"}
+            id={uniqueID+"reset"}
             padding="dense"
-            checked={this.state.resetResponses}
+            checked={resetResponses}
             control={<Checkbox  color="secondary" />}
-            onChange={this.handleResetResponsesChanged}
+            onChange={onResetResponsesChanged}
             labelPlacement="end"
           />
           <FormControlLabel label="Make Global Variable"
             value="end"
             padding="dense"
-            id={this.uniqueID+"globalVar"}
-            checked={this.state.globalVariable}
+            id={uniqueID+"globalVar"}
+            checked={globalVariable}
             control={<Checkbox color="secondary" />}
-            onChange={this.handleGlobalVariableChanged}
+            onChange={onGlobalVariableChanged}
             labelPlacement="end"
           />
           <TextField label="Screen IDs"
             required
             padding="dense"
             style={{width:"100px"}}
-            id={this.uniqueID+"mcScreenIDs"}
-            defaultValue={this.props.task.screenIDS.join(',')}
+            id={uniqueID+"mcScreenIDs"}
+            defaultValue={props.task.screenIDS.join(',')}
             placeholder="1, 2"
-            ref="mcScreenIDRef"
-            onChange={(e)=> this.responseHandler(e, e.target.value, "ScreenIDs")}
+            onChange={(e)=> onResponsesChanged(e, e.target.value, "ScreenIDs")}
           />
         </div>;
         break;
@@ -207,42 +171,39 @@ class TaskComponentItem extends Component {
             required
             padding="dense"
             fullWidth
-            id={this.uniqueID+"numQuestionText"}
-            defaultValue={this.props.task.displayText}
+            id={uniqueID+"numQuestionText"}
+            defaultValue={props.task.displayText}
             placeholder="Enter your question here"
-            ref="questionTextRef"
             multiline
             rows="1"
-            onChange={(e)=>{this.props.task.displayText = e.target.value}}
+            onChange={(e)=>{props.task.displayText = e.target.value}}
           />
           <TextField label="Correct answer, +-Margin(Optional)"
             required
             padding="dense"
             style={{marginRight:"10px", width:"calc(40% - 15px)"}}
-            id={this.uniqueID+"numCorrectResponses"}
-            defaultValue={this.props.task.correctResponses.join(',')}
+            id={uniqueID+"numCorrectResponses"}
+            defaultValue={props.task.correctResponses.join(',')}
             placeholder="3, 0.2"
-            ref="correctResponseRef"
-            onChange={(e)=> this.responseHandler(e, e.target.value, "Correct Responses")}
+            onChange={(e)=> onResponsesChanged(e, e.target.value, "Correct Responses")}
           />
           <FormControlLabel label="Make Global Variable"
             value="end"
             padding="dense"
-            id={this.uniqueID+"globalVar"}
-            checked={this.state.globalVariable}
+            id={uniqueID+"globalVar"}
+            checked={globalVariable}
             control={<Checkbox color="secondary" />}
-            onChange={this.handleGlobalVariableChanged}
+            onChange={onGlobalVariableChanged}
             labelPlacement="end"
           />
           <TextField label="Screen IDs"
              required
              padding="dense"
              style={{width:"100px"}}
-             id={this.uniqueID+"numScreenIDs"}
-             defaultValue={this.props.task.screenIDS.join(',')}
+             id={uniqueID+"numScreenIDs"}
+             defaultValue={props.task.screenIDS.join(',')}
              placeholder="1, 2"
-             ref="numScreenIDRef"
-             onChange={(e)=> this.responseHandler(e, e.target.value, "ScreenIDs")}
+             onChange={(e)=> onResponsesChanged(e, e.target.value, "ScreenIDs")}
            />
         </div>;
         break;
@@ -253,41 +214,38 @@ class TaskComponentItem extends Component {
             required
             padding="dense"
             fullWidth
-            id={this.uniqueID+"textQuestionText"}
-            defaultValue={this.props.task.displayText}
+            id={uniqueID+"textQuestionText"}
+            defaultValue={props.task.displayText}
             placeholder="Enter your question here"
-            ref="questionTextRef"
             multiline
             rows="1"
-            onChange={(e)=>{this.props.task.displayText = e.target.value}}
+            onChange={(e)=>{props.task.displayText = e.target.value}}
           />
           <TextField label="Correct answer"
             padding="dense"
             style={{marginRight:"10px", width:"calc(40% - 15px)"}}
-            id={this.uniqueID+"textCorrectResponses"}
-            defaultValue={this.props.task.correctResponses.join(',')}
+            id={uniqueID+"textCorrectResponses"}
+            defaultValue={props.task.correctResponses.join(',')}
             placeholder="Answer A, Answer C"
-            ref="correctResponseRef"
-            onChange={(e)=> this.responseHandler(e, e.target.value, "Correct Responses")}
+            onChange={(e)=> onResponsesChanged(e, e.target.value, "Correct Responses")}
           />
           <FormControlLabel label="Make Global Variable"
             value="end"
             padding="dense"
-            id={this.uniqueID+"globalVar"}
-            checked={this.state.globalVariable}
+            id={uniqueID+"globalVar"}
+            checked={globalVariable}
             control={<Checkbox color="secondary" />}
-            onChange={this.handleGlobalVariableChanged}
+            onChange={onGlobalVariableChanged}
             labelPlacement="end"
           />
           <TextField label="Screen IDs"
              required
              padding="dense"
              style={{width:"100px"}}
-             id={this.uniqueID+"textScreenIDs"}
-             defaultValue={this.props.task.screenIDS.join(',')}
+             id={uniqueID+"textScreenIDs"}
+             defaultValue={props.task.screenIDS.join(',')}
              placeholder="1, 2"
-             ref="textScreenIDRef"
-             onChange={(e)=> this.responseHandler(e, e.target.value, "ScreenIDs")}
+             onChange={(e)=> onResponsesChanged(e, e.target.value, "ScreenIDs")}
            />
         </div>;
         break;
@@ -298,21 +256,17 @@ class TaskComponentItem extends Component {
     return component;
   }
 
-  onSingleChoiceChanged(e, checked){
-    this.props.task.singleChoice = checked;
-    this.setState({
-      singleChoice: checked,
-    });
+  const onSingleChoiceChanged = (e, checked) => {
+    props.task.singleChoice = checked;
+    setSingleChoice(checked);
   }
 
-  onResetResponsesChanged(e, checked){
-    this.props.task.resetResponses = checked;
-    this.setState({
-      resetResponses: checked,
-    });
+  const onResetResponsesChanged = (e, checked) => {
+    props.task.resetResponses = checked;
+    setResetResponses(checked);
   }
 
-  onResponsesChanged(e, response, target){
+  const onResponsesChanged = (e, response, target) => {
     response = response.replace(/\s+/g, " ");
     response = response.trim();
     response = response.split(",");
@@ -322,22 +276,20 @@ class TaskComponentItem extends Component {
     response = response.filter(Boolean); //Remove empty values
 
     if(target==="Responses"){
-      this.props.task.responses = response;
+      props.task.responses = response;
     }
     else if(target==="Correct Responses"){
-      this.props.task.correctResponses = response;
+      props.task.correctResponses = response;
     }
     else if(target==="ScreenIDs"){
-      this.props.task.screenIDS = response;
+      props.task.screenIDS = response;
     }
   }
 
-  render() {
-    var task = this.getComponent();
-    return(
-      <div style={{marginRight:20, marginTop:10, marginBottom: 10}}> {task} </div>
-    );
-  }
+  const task = getComponent();
+  return(
+    <div style={{marginRight:20, marginTop:10, marginBottom: 10}}> {task} </div>
+  );
 }
 
 export default TaskComponentItem;
