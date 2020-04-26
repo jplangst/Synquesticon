e480ea505a86f7c91300a30fb2b2439135d6c044
michaelhil <51119446+michaelhil@users.noreply.github.com>
2020-04-26 23:36:41 +0200

    Update NumpadComponent.js

--- WebEntry/src/components/PlayerComponents/Views/NumpadComponent.js
+++ WebEntry/src/components/PlayerComponents/Views/NumpadComponent.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 
 import { Typography } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
@@ -12,110 +12,102 @@ const second_line_keyboard = [4, 5, 6]
 const third_line_keyboard = [7, 8, 9]
 const fourth_line_keyboard = [0, '.', "<"]
 
-class NumpadComponent extends Component {
-  constructor() {
-    super();
-    this.textEntry = "";
-    this.keyboardPressed = this.onMyKeyboardPressed.bind(this);
-    this.decimalWasPressed = false;
-    this.textRef = React.createRef();
-  }
+let textEntry = "";
+let decimalWasPressed = false;
+const textRef = React.createRef();
 
-  componentDidMount() {
-    var textAOIAction = {
+const NumpadComponent = props => {
+  useEffect ( () => {
+    const textAOIAction = {
       type: 'ADD_AOIS',
       aois: {
-        name: this.props.parentSet + '_' + this.props.task.displayText,
+        name: props.parentSet + '_' + props.task.displayText,
         boundingbox: [],
-        imageRef: this.textRef
+        imageRef: textRef
       }
     }
     store.dispatch(textAOIAction);
-  }
+  }, []);
 
-  onMyKeyboardPressed(key) {
+  const onMyKeyboardPressed = key => {
     if (key === "<") {
-      var lastChar = this.textEntry[this.textEntry.length -1];
+      var lastChar = textEntry[textEntry.length -1];
       if (lastChar === '.') {
-        this.decimalWasPressed = false;
+        decimalWasPressed = false;
       }
-      this.textEntry = this.textEntry.substring(0, this.textEntry.length-1);
+      textEntry = textEntry.substring(0, textEntry.length-1);
     }
     else if (key === '.') {
-      if (!this.decimalWasPressed) {
-        this.textEntry += key;
-        this.decimalWasPressed = true;
+      if (!decimalWasPressed) {
+        textEntry += key;
+        decimalWasPressed = true;
       }
+    } else {
+      textEntry = textEntry + key;
     }
-    else {
-      //this.textEntry = parseFloat(this.textEntry + key) + "";
-      this.textEntry = this.textEntry + key;
-    }
-    this.onAnswer();
+    onAnswer();
   }
 
-  checkAnswer() {
-    if (this.props.task.correctResponses === undefined || this.props.task.correctResponses.length === 0) {
+  function checkAnswer() {
+    if (props.task.correctResponses === undefined || props.task.correctResponses.length === 0) {
       return "notApplicable";
     }
 
     //If the response has two values then we treat the second as how much the answer can differ and still be valid
-    if(this.props.task.correctResponses.length > 1){
-      let answer = parseFloat(this.textEntry);
-      let correctAnswer = parseFloat(this.props.task.correctResponses[0]);
-      let threshold = parseFloat(this.props.task.correctResponses[1]);
+    if(props.task.correctResponses.length > 1){
+      let answer = parseFloat(textEntry);
+      let correctAnswer = parseFloat(props.task.correctResponses[0]);
+      let threshold = parseFloat(props.task.correctResponses[1]);
       if(answer >= correctAnswer-threshold && answer <= correctAnswer+threshold){
         return "correct";
       }
     } //Otherwise we just check if it matches the correct response
-    else if(this.props.task.correctResponses.length === 1){
-      if(parseFloat(this.props.task.correctResponses[0]) === parseFloat(this.textEntry)) {
+    else if(props.task.correctResponses.length === 1){
+      if(parseFloat(props.task.correctResponses[0]) === parseFloat(textEntry)) {
         return "correct";
       }
     }
     return "incorrect";
   }
 
-  onAnswer() {
-    var answerObj = {
-      responses: [parseFloat(this.textEntry)],
-      correctlyAnswered: this.checkAnswer(),
-      taskID: this.props.task._id,
-      mapID: this.props.mapID,
+  function onAnswer() {
+    const answerObj = {
+      responses: [parseFloat(textEntry)],
+      correctlyAnswered: checkAnswer(),
+      taskID: props.task._id,
+      mapID: props.mapID,
     }
-    this.props.answerCallback(answerObj);
+    props.answerCallback(answerObj);
   }
 
-  render() {
-    var getKeyboardLine = (keyboard, css) => {
-      return (<div className={css}>
-              {keyboard.map((item, index) => {
-                  return <span className="inputButton" key={index}>
-                            <Button key={index} variant="contained" onClick={() => this.keyboardPressed(item)}>
-                              <Typography variant='body1' align='center'>{item}</Typography>
-                            </Button>
-                         </span>
-                  }
-                )
-              }
-              </div>);
-    }
+  const getKeyboardLine = (keyboard, css) => {
+    return (<div className={css}>
+            {keyboard.map((item, index) => {
+                return <span className="inputButton" key={index}>
+                          <Button key={index} variant="contained" onClick={() => onMyKeyboardPressed(item)}>
+                            <Typography variant='body1' align='center'>{item}</Typography>
+                          </Button>
+                        </span>
+                }
+              )
+            }
+            </div>);
+  }
 
-    return (
-      <div className={this.props.className} >
-        <div>
-          <Typography ref={this.textRef} variant="h3" align="center" style={{whiteSpace:"pre-line"}} color="textPrimary">{this.props.displayText}</Typography>
-        </div>
-        <div className="inputField">
-          <Typography color="textPrimary">{this.textEntry}</Typography>
-        </div>
-        {getKeyboardLine(first_line_keyboard, "firstLine")}
-        {getKeyboardLine(second_line_keyboard, "firstLine")}
-        {getKeyboardLine(third_line_keyboard, "firstLine")}
-        {getKeyboardLine(fourth_line_keyboard, "thirdLine")}
+  return (
+    <div className={props.className} >
+      <div>
+        <Typography ref={textRef} variant="h3" align="center" style={{whiteSpace:"pre-line"}} color="textPrimary">{props.displayText}</Typography>
       </div>
-    );
-  }
+      <div className="inputField">
+        <Typography color="textPrimary">{textEntry}</Typography>
+      </div>
+      {getKeyboardLine(first_line_keyboard, "firstLine")}
+      {getKeyboardLine(second_line_keyboard, "firstLine")}
+      {getKeyboardLine(third_line_keyboard, "firstLine")}
+      {getKeyboardLine(fourth_line_keyboard, "thirdLine")}
+    </div>
+  );
 }
 
 export default NumpadComponent;
