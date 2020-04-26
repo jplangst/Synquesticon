d1e8595cec2916083e6a888ec602cda4c4c918e3
michaelhil <51119446+michaelhil@users.noreply.github.com>
2020-04-26 16:20:27 +0200

    Revert "Update FilterDialog.js"
    This reverts commit b2cbbc0907c807eba73948e7ccbd073c22e7be16.

--- WebEntry/src/EditMode/FilterDialog.js
+++ WebEntry/src/EditMode/FilterDialog.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 
 import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContent from '@material-ui/core/DialogContent';
@@ -12,53 +12,66 @@ import Button from '@material-ui/core/Button';
 
 import db_helper from '../core/db_helper';
 
-const FilterDialog = props => {
-  const [filters, setFilters] = useState([]);
-  const [pickedFilters, setPickedFilters] = useState(props.filterObject.tagFilters ? props.filterObject.tagFilters: []);
-  const [queryCombination, setQueryCombination] = useState(props.filterObject.queryCombination ? props.filterObject.queryCombination : "OR");
-  const [filterType, setFilterType] = useState(null);
+class FilterDialog extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      filters: [],
+      pickedFilters: props.filterObject.tagFilters ? props.filterObject.tagFilters: [],
+      queryCombination: props.filterObject.queryCombination ? props.filterObject.queryCombination : "OR",
+      filterType:null
+    }
 
-  if(props.filterType){
-    db_helper.queryAllTagValuesFromDB(props.filterType, (type, result)=>{
-      setFilters(result.tags)
-      setFilterType(props.filterType);
-    });
+    if(this.props.filterType){
+      db_helper.queryAllTagValuesFromDB(this.props.filterType, (type, result)=>{
+        this.setState({filters:result.tags, filterType:this.props.filterType});
+      });
+    }
   }
 
-  const onOKAction = () => {
+  onOKAction() {
     //Callback to editor
-    props.onFiltersUpdated(props.filterType, pickedFilters, queryCombination);
+    this.props.onFiltersUpdated(this.props.filterType, this.state.pickedFilters, this.state.queryCombination);
   }
 
-  const onCancelAction = () => {
-    props.closeDialog();
+  onCancelAction(){
+
+    this.props.closeDialog();
   }
 
-  const onClearAction = () => {
-    setPickedFilters([]);
+  onClearAction(){
+    this.setState({pickedFilters:[]});
   }
 
-  const onPickFilter = filter => {
+  onPickFilter(filter) {
     //Since we can't mutate the state directly
-    let filtersCopy = pickedFilters.slice();
+    let filtersCopy = this.state.pickedFilters.slice();
 
-    if(pickedFilters.includes(filter)){
-      let index = pickedFilters.indexOf(filter);
+    if(this.state.pickedFilters.includes(filter)){
+      let index = this.state.pickedFilters.indexOf(filter);
       filtersCopy.splice(index, 1)
-    } else{
+    }
+    else{
       filtersCopy.push(filter);
     }
 
-    setPickedFilters(filtersCopy);
+    this.setState({
+      pickedFilters: filtersCopy
+    });
   }
 
-    const buttonContainerHeight = 60;
-    const buttonHeight = buttonContainerHeight - 4;
+  onQueryCombinationChanged(queryType){
+    this.setState({queryCombination: queryType});
+  }
 
-    let content = filters.map((filter, ind) => {
-      let borderColor = pickedFilters.includes(filter)?"secondary":"default";
+  render() {
+    var buttonContainerHeight = 60;
+    var buttonHeight = buttonContainerHeight - 4;
+
+    let content = this.state.filters.map((filter, ind) => {
+      let borderColor = this.state.pickedFilters.includes(filter)?"secondary":"default";
       return(
-        <Button key={ind} style={{margin: 5, width:120, minWidth:0, minHeight:0, height: 60}} onClick={onPickFilter.bind(this,filter)}
+        <Button key={ind} style={{margin: 5, width:120, minWidth:0, minHeight:0, height: 60}} onClick={this.onPickFilter.bind(this,filter)}
           variant="outlined" color={borderColor}>
             {filter}
         </Button>
@@ -67,13 +80,13 @@ const FilterDialog = props => {
 
     return(
       <Dialog
-          open={props.openDialog}
-          onClose={onCancelAction}
+          open={this.props.openDialog}
+          onClose={this.onCancelAction.bind(this)}
           aria-labelledby="form-dialog-title"
           fullWidth={true}
           maxWidth='md'
         >
-          <DialogTitle style={{height:30}} id="form-dialog-title">{props.filterType.substring(0, props.filterType.length - 1)} Tags</DialogTitle>
+          <DialogTitle style={{height:30}} id="form-dialog-title">{this.props.filterType.substring(0, this.props.filterType.length - 1)} Tags</DialogTitle>
           <DialogContent style={{display:'flex', flexDirection:'row',
                            minHeight:100, maxHeight:'90%', maxWidth:'100%', overflowY:'auto'}}>
             <div style={{width:'100%', height:'100%'}}>{ content }</div>
@@ -83,27 +96,28 @@ const FilterDialog = props => {
                style={{whiteSpace:"pre-line"}}>
                Query type
             </Typography>
-            <Button style={{height:buttonHeight}} variant="outlined" color={queryCombination==="AND"?"secondary":"default"}
-                onClick={() => setQueryCombination("AND")}>
+            <Button style={{height:buttonHeight}} variant="outlined" color={this.state.queryCombination==="AND"?"secondary":"default"}
+                onClick={this.onQueryCombinationChanged.bind(this, "AND")}>
               AND
             </Button>
-            <Button style={{height:buttonHeight}} variant="outlined" color={queryCombination==="OR"?"secondary":"default"}
-                onClick={() => setQueryCombination("OR")}>
+            <Button style={{height:buttonHeight}} variant="outlined" color={this.state.queryCombination==="OR"?"secondary":"default"}
+                onClick={this.onQueryCombinationChanged.bind(this, "OR")}>
               OR
             </Button>
             <div style={{flexGrow:1}}/>
-            <Button style={{height:buttonHeight}} onClick={onCancelAction} variant="outlined">
+            <Button style={{height:buttonHeight}} onClick={this.onCancelAction.bind(this)} variant="outlined">
               CANCEL
             </Button>
-            <Button style={{height:buttonHeight}} onClick={onClearAction} variant="outlined">
+            <Button style={{height:buttonHeight}} onClick={this.onClearAction.bind(this)} variant="outlined">
               Clear
             </Button>
-            <Button style={{height:buttonHeight}} onClick={onOKAction} variant="outlined">
+            <Button style={{height:buttonHeight}} onClick={this.onOKAction.bind(this)} variant="outlined">
               OK
             </Button>
           </DialogActions>
       </Dialog>
     );
+  }
 }
 
 export default withTheme(FilterDialog);
