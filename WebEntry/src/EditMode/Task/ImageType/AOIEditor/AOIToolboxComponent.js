5d9d985282ba1836bcf769e65be707ae1cb2a847
michaelhil <51119446+michaelhil@users.noreply.github.com>
2020-04-26 15:21:55 +0200

    Update AOIToolboxComponent.js

--- WebEntry/src/EditMode/Task/ImageType/AOIEditor/AOIToolboxComponent.js
+++ WebEntry/src/EditMode/Task/ImageType/AOIEditor/AOIToolboxComponent.js
@@ -1,65 +1,55 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import { withTheme } from '@material-ui/styles';
-import { Typography } from '@material-ui/core';
+
 
 import PolygonToolBox from './PolygonToolBox';
 import SelectAOIToolBox from './SelectAOIToolBox';
 
 import './AOIEditorComponent.css';
 
-class AOIToolboxComponent extends Component {
-  constructor(props) {
-    super(props);
-
-    this.handleTabs = this.handleChangeTab.bind(this);
+const AOIToolboxComponent = props => {
+  const handleChangeTab = (evt, mode) => {
+    props.onSwitchMode(mode);
   }
 
-  handleChangeTab(evt, mode) {
-    this.props.onSwitchMode(mode);
+  let theme = props.theme;
+  let toolbox = null;
+
+  switch (props.mode) {
+    case "RECTANGLE":
+      break;
+    case "POLYGON":
+      toolbox = <PolygonToolBox onRemoveLastPoint={props.callbacks.onRemoveLastPoint}
+                                onFinished={props.callbacks.onFinished}
+                                onCancel={props.callbacks.onCancel}/>
+      break;
+    case "SELECT":
+      toolbox = <SelectAOIToolBox onRename={props.callbacks.onRename}
+                                  onRemove={props.callbacks.onRemove}/>
+      break;
+    default:
+      break;
   }
 
-  render() {
-    let theme = this.props.theme;
-
-
-    var toolbox = null;
-    switch (this.props.mode) {
-      case "RECTANGLE":
-        break;
-      case "POLYGON":
-        toolbox = <PolygonToolBox onRemoveLastPoint={this.props.callbacks.onRemoveLastPoint}
-                                  onFinished={this.props.callbacks.onFinished}
-                                  onCancel={this.props.callbacks.onCancel}/>
-        break;
-      case "SELECT":
-        toolbox = <SelectAOIToolBox onRename={this.props.callbacks.onRename}
-                                    onRemove={this.props.callbacks.onRemove}/>
-        break;
-      default:
-        break;
-    }
-
-    return (
-      <div style={{display:'flex', flexDirection:'column', alignItems:'center'}}>
-        <Tabs
-          value={this.props.mode}
-          onChange={this.handleTabs}
-          indicatorColor="secondary"
-          variant="fullWidth"
-          style={{color:theme.palette.text.primary, marginBottom:5}}
-        >
-            <Tab label="Rectangle" value="RECTANGLE" />
-            <Tab label="Polygon" value="POLYGON" />
-            <Tab label="Select" value="SELECT" />
-        </Tabs>
-        <div className="supportingToolBox">{toolbox}</div>
-      </div>
-    );
-  }
-
+  return (
+    <div style={{display:'flex', flexDirection:'column', alignItems:'center'}}>
+      <Tabs
+        value={props.mode}
+        onChange={handleChangeTab}
+        indicatorColor="secondary"
+        variant="fullWidth"
+        style={{color:theme.palette.text.primary, marginBottom:5}}
+      >
+          <Tab label="Rectangle" value="RECTANGLE" />
+          <Tab label="Polygon" value="POLYGON" />
+          <Tab label="Select" value="SELECT" />
+      </Tabs>
+      <div className="supportingToolBox">{toolbox}</div>
+    </div>
+  );
 }
 
 export default withTheme(AOIToolboxComponent);
